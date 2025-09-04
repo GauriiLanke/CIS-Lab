ac1da876ac5b3937a4c1efd8029c93dcc2be5cf8
GauriiLanke <gaurilanke24@gmail.com>
2025-09-04 11:13:03 +0530

    Updated Code

--- Python Codes/Diffie_hellman_keyExchange.py
+++ Python Codes/Diffie_hellman_keyExchange.py
@@ -1,81 +1,30 @@
 # # Diffie-Hellman Key Exchange Algorithm
 
-# p = int(input("Enter a prime number (p): "))
-# g = int(input("Enter a primitive root (g): "))
-
-# # Private keys 
-# a = int(input("Enter User A private key (a): "))
-# b = int(input("Enter User B private key (b): "))
-
-# # Public keys
-# A = pow(g, a, p)  # (g^a) mod p
-# B = pow(g, b, p)  # (g^b) mod p
-
-# print(f"\nUser A Public Key (A): {A}")
-# print(f"User B Public Key (B): {B}")
-
-# # Secret keys
-# secret_A = pow(B, a, p)  # (B^a) mod p
-# secret_B = pow(A, b, p)  # (A^b) mod p
-
-# print(f"\nAlice's Shared Secret: {secret_A}")
-# print(f"Bob's Shared Secret: {secret_B}")
-
-# if secret_A == secret_B:
-#     print("\nKey Exchange Successful! Shared secret established.")
-# else:
-#     print("\nKey Exchange Failed!")
-
-
-# Diffie-Hellman Key Exchange Algorithm with Auto Primitive Root
-
-# Calculate GCD 
-def gcd(a, b):
-    while b:
-        a, b = b, a % b
-    return a
-
-# Find primitive root of prime p
-def find_primitive_root(p):
-    # The set of numbers from 1 to p-1 that are relatively prime to p
-    required_set = set(num for num in range(1, p) if gcd(num, p) == 1)
-    for g in range(2, p):  # Primitive roots are in range [2, p-1]
-        actual_set = set(pow(g, powers, p) for powers in range(1, p))
-        if required_set == actual_set:
-            return g
-    return None
-
 print("\nDiffie-Hellman Key Exchange Algorithm\n")
-
-p = int(input("Enter a prime number (p): "))
-
-# Primitive roots
-g = find_primitive_root(p)
-if g is None:
-    print("No primitive root found for the given prime.")
-    exit()
-else:
-    print(f"Primitive root (g) : {g}")
+q = int(input("Enter a prime number (q): "))
+alpha = int(input("Enter a primitive root (alpha): "))
 
 # Private keys 
 a = int(input("\nEnter User A private key (a): "))
 b = int(input("Enter User B private key (b): "))
 
 # Public keys
-A = pow(g, a, p)  # (g^a) mod p
-B = pow(g, b, p)  # (g^b) mod p
+A = pow(alpha, a, q)  # (apha^a) mod p
+B = pow(alpha, b, q)  # (apha^b) mod p
 
 print(f"\nUser A Public Key (A): {A}")
 print(f"User B Public Key (B): {B}")
 
 # Secret keys
-secret_A = pow(B, a, p)  # (B^a) mod p
-secret_B = pow(A, b, p)  # (A^b) mod p
+secret_A = pow(B, a, q)  # (B^a) mod q
+secret_B = pow(A, b, q)  # (A^b) mod q
 
-print(f"\nUser A's Shared Secret Key: {secret_A}")
-print(f"User B's Shared Secret Key: {secret_B}")
+print(f"\nAlice's Shared Secret Key: {secret_A}")
+print(f"Bob's Shared Secret Key: {secret_B}")
 
 if secret_A == secret_B:
     print("\nKey Exchange Successful! Shared secret established.")
 else:
-    print("\nKey Exchange Failed!")
+    print("\nKey Exchange Failed!")
+
+
